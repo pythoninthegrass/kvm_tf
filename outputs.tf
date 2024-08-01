2a04602c3af9cfd16d5ed449a7e5e20977935147
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-08-01 14:20:53 -0500

    chore: remove debug output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "ip" {
 output "url" {
   value = "http://${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}"
 }
-
-output "debug_uri" {
-  value = "qemu+ssh://${local.server_user}@${local.server_host}/system"
-}
